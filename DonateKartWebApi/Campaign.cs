4142d2ad894850692edda25d67e20e06060d87a7
Shashank Shekhar <shashanks@zenoti.com>
2021-10-25 09:43:04 +0530

    initial commit

---
+++ DonateKartWebApi/Campaign.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DonateKartWebApi
+{
+    public class Campaign
+    {
+        public string code { get; set; }
+        public string title { get; set; }
+        public string featured { get; set; }
+        public string priority { get; set; }
+        public string shortDesc { get; set; }
+        public string ImageSrc { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? EndDate { get; set; }
+        public long? TotalAmount { get; set; }
+        public long? ProcuredAmount { get; set; }
+        public long? TotalProcured { get; set; }
+        public long? BackersCount { get; set; }
+        public int CategoryId { get; set; }
+        public string Location { get; set; }
+        public string NgoCode { get; set; }
+        public string NgoName { get; set; }
+        public int? DaysLeft { get; set; }
+        public float? Percentage { get; set; }
+
+    }
+
+    public class CampaignShortData
+    {
+        public string title { get; set; }
+        public DateTime? EndDate { get; set; }
+        public long? TotalAmount { get; set; }
+        public long? BackersCount { get; set; }
+
+    }
+}
---
+++ DonateKartWebApi/Controllers/DonateKartController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DonateKartWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DonateKartController : ControllerBase
+    {
+        private readonly ILogger<DonateKartController> _logger;
+
+        public DonateKartController(ILogger<DonateKartController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<List<CampaignShortData>> GetCampaign()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://testapi.donatekart.com/api/campaign"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    var res = JsonConvert.DeserializeObject<List<CampaignShortData>>(apiResponse);
+                    return res;
+                }
+            }
+        }
+
+        [HttpGet]
+        [Route("active")]
+        public async Task<List<Campaign>> GetActiveCampaignCreatedInLastOneMonth()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://testapi.donatekart.com/api/campaign"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    var res = JsonConvert.DeserializeObject<List<Campaign>>(apiResponse);
+                    res = res.Where(i => i.EndDate > DateTime.Now && i.Created > DateTime.Today.AddDays(-30)).ToList();
+                    return res;
+                }
+            }
+        }
+
+        [HttpGet]
+        [Route("inactive")]
+        public async Task<List<Campaign>> GetAllInactiveCampaign()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://testapi.donatekart.com/api/campaign"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    var res = JsonConvert.DeserializeObject<List<Campaign>>(apiResponse);
+                    res = res.Where(i => i.EndDate < DateTime.Now || i.ProcuredAmount >= i.TotalAmount ).ToList();
+                    return res;
+                }
+            }
+        }
+    }
+}
